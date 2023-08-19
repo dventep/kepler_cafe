befbdd096707fda4936852d8eefa9a1f42bd831e
lapiceroazul4 <lapiceroazul@proton.me>
2023-08-19 17:39:56 -0500

    Prueba de tabla productos

--- developing/kepler_cafe/Inventory/views.py
+++ developing/kepler_cafe/Inventory/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.contrib.auth.decorators import login_required
+from .models import Product
 
 # Create your views here.
-def aguita(request):
-    return HttpResponse("holaMundo(print)")
+
+
+@login_required
+def inventory_list(request):
+    inventory = Product.objects.all()
+    context = {
+        'tittle': 'Inventory List',
+        'Inventory': inventory
+
+    }
+    return render(request, 'inventory/Producto/vistaPrincipalProductos.html', context=context)
